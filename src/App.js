53d723dcf86d374f1a1f5c6c93d033eb37841ece
elidvenega <venegaelid@gmail.com>
2022-07-31 16:05:20 -0700

    updated Packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import HookCounter from './components/HookCounter';
+import HookCounter from "./components/HookCounter";
 // import HookCounterTwo from './components/HookCounterTwo';
 // import HookCounterThree from './components/HookCounterThree';
 // import HookCounterFour from './components/HookCounterFour';
@@ -8,7 +8,7 @@ import React from "react";
 // import UseEffectOne from './components/UseEffectOne';
 //import EffectTutorial from './components/EffectTutorial';
 //import Props from './components/Props';
-import RefTutorial from "./components/RefTutorial";
+// import RefTutorial from "./components/RefTutorial";
 import "./App.css";
 
 function App() {
@@ -16,7 +16,8 @@ function App() {
     <div className="App">
       {/* <UseEffectOne /> */}
       {/* <EffectTutorial /> */}
-      <RefTutorial />
+      {/* <RefTutorial /> */}
+      <HookCounter />
       {/* <Props name="Jake" />
       <Props name="John" />
       <Props name="Mike" /> */}
