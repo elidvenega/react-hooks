e9fc7fa6d84256b69c6d642571c90c914ea19ae7
elidvenega <venegaelid@gmail.com>
2022-09-20 16:00:52 -0700

    Updated

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -1,17 +1,19 @@
-import React, {useState} from 'react';
-import './css/style.css';
+import React, { useState } from "react";
+import "./css/style.css";
 
 function HookCounter() {
+  const [number, setNumber] = useState(0);
 
-    const [number,count] = useState(0);
-
-    const addingByOne = () => count(number + 1);
-    return (
-        <div>
-            <h1>Hook App</h1>
-            <button className="btn" onClick={() => addingByOne()}>Click {number}</button>
-        </div>
-    )
+  const addingByOne = () => setNumber(number + 1);
+  return (
+    <div>
+      <h1>Hook App</h1>
+      <p>{number}</p>
+      <button className="btn" onClick={() => addingByOne()}>
+        Click 
+      </button>
+    </div>
+  );
 }
 
 export default HookCounter;
