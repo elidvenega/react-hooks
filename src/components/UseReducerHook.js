b4b3f64eb5d31eff1f4905142b8610ab065d5d3e
elidvenega <venegaelid@gmail.com>
2022-10-30 21:19:03 -0700

    Updated

--- src/components/UseReducerHook.js
+++ src/components/UseReducerHook.js
@@ -1,27 +1,81 @@
-import React, { useState,useReducer } from "react";
+import React, { useState, useReducer } from "react";
 
-function reducer(state, action) {
-  return { count: state.count + 1 };
+/* Complex Way of using useReducer */
+// const ACTIONS = {
+//   INCREMENT: 'increment',
+//   DECREMENT: 'decrement'
+// }
+
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case ACTIONS.INCREMENT:
+//       return { count: state.count + 1 };
+//     case ACTIONS.DECREMENT:
+//       return { count: state.count - 1 };
+//     default:
+//       return state;
+//   }
+// }
+
+// export default function UseReducerHook() {
+//   const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+//   function increment() {
+//     dispatch({ type: ACTIONS.INCREMENT });
+//   }
+
+//   function decrement() {
+//     dispatch({ type: ACTIONS.DECREMENT});
+//   }
+
+//   return (
+//     <>
+//       <button onClick={decrement}>-</button>
+//       <span>{state.count}</span>
+//       <button onClick={increment}>+</button>
+//     </>
+//   );
+// }
+
+const ACTIONS = {
+  ADD_TODO: "add-todo",
+};
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)];
+  }
+}
+
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false };
 }
 
 export default function UseReducerHook() {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
-  dispatch()
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState("");
 
-  function increment() {
-    dispatch()
+  function handleSubmit(e) {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.ADD_TODO, payload: { name: name } });
+    setName("");
   }
 
-  function decrement() {
-  // setCount((prevCount) => prevCount - 1);
-   }
+  console.log(todos);
 
   return (
     <>
-      <button onClick={decrement}>-</button>
-      <span>{state.count}</span>
-      <button onClick={increment}>+</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          target="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
     </>
   );
 }
+
+
 
